56640aaff3be1562c07bd9dc5cbccae2fa4dff1e
Dhrumit Patel <drpatel@sigmasolve.net>
2020-01-28 22:16:29 +0530

    Initial Commit

---
+++ Libraries/Nop.Core/Domain/Employees/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Nop.Core.Domain.Employees
+{
+    public class Employee : BaseEntity
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public decimal? Salary { get; set; }
+
+        public Employee()
+        {
+            EmployeeGuid = Guid.NewGuid();
+        }
+
+        public Guid EmployeeGuid { get; set; }
+    }
+}
---
+++ Libraries/Nop.Data/CustomNopMapping/Employees/EmployeeMap.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nop.Core.Domain.Employees;
+using Nop.Data.Mapping;
+
+namespace Nop.Data.CustomNopMapping.Employees
+{
+    public class EmployeeMap : NopEntityTypeConfiguration<Employee>
+    {
+        #region Methods
+
+        /// <summary>
+        /// Configures the entity
+        /// </summary>
+        /// <param name="builder">The builder to be used to configure the entity</param>
+        public override void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.ToTable(nameof(Employee));
+            builder.HasKey(employee => employee.Id);
+            builder.Property(employee => employee.Name).HasMaxLength(500);
+        }
+
+        #endregion
+    }
+}
